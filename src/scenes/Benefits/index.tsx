b4c1814764a8cd2658da8861e2a3d1425d092eb4
ViniVanC <vincentvan0302@gmail.com>
2023-03-07 21:13:21 +0200

    commit

---
+++ src/scenes/Benefits/index.tsx
@@ -0,0 +1,177 @@
+import ActionButton from "@/shared/ActionButton";
+import HText from "@/shared/HText";
+import { BenefitType, SelectedPage } from "@/shared/types";
+import {
+  HomeModernIcon,
+  UserGroupIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
+import Benefit from "./Benefit";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description:
+      "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description:
+      "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description:
+      "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
+  },
+];
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const Benefits = ({ setSelectedPage }: Props) => {
+  return (
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        {/* header */}
+        <motion.div
+          className="md:my-5 md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HText>MORE THAN JUST GUM.</HText>
+          <p className="my-5 text-sm">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti
+            quasi perferendis debitis repellat, iste voluptatum! Dolores
+            similique iste mollitia totam dolorum praesentium accusamus magni,
+            quidem iure incidunt eaque, dolorem quam.
+          </p>
+        </motion.div>
+
+        {/* benefits */}
+        <motion.div
+          className="items-center justify-between gap-8 md:flex"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
+        </motion.div>
+
+        {/* graphics and description */}
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+          {/* graphics */}
+          <img
+            className="mx-auto"
+            src={BenefitsPageGraphic}
+            alt="benefits-page-graphic"
+          />
+
+          {/* description */}
+          <div className="">
+            {/* title */}
+            <div className="relative">
+              <div className="brfore:-left-20 before:-top-20 before:z-[1] before:content-abstractWaves">
+                <motion.div
+                  className="relative"
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: 100 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                >
+                  <HText>
+                    MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                    <span className="text-primary-500">FIT</span>
+                  </HText>
+                </motion.div>
+              </div>
+            </div>
+
+            {/* descript */}
+            <div>
+              <motion.p
+                className="my-5"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ delay: 0.2, duration: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, x: -100 },
+                  visible: { opacity: 1, x: 0 },
+                }}
+              >
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Commodi necessitatibus eligendi, natus eum inventore molestias.
+                Eligendi labore obcaecati nobis quasi, quis dolores laborum
+                laboriosam, temporibus porro necessitatibus optio hic sapiente.
+              </motion.p>
+              <motion.p
+                className="mb-5"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ delay: 0.4, duration: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, x: 100 },
+                  visible: { opacity: 1, x: 0 },
+                }}
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+                totam dicta quibusdam. Maxime sit assumenda temporibus ex eaque
+                ea praesentium pariatur, inventore quas cum esse eius quisquam
+                deleniti in fugit?
+              </motion.p>
+            </div>
+
+            {/* button */}
+            <div className="relative mt-16">
+              <div className="before:absolute before:right-40 before:-bottom-20 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+              </div>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Benefits;
---
+++ src/scenes/Navbar/index.tsx
@@ -0,0 +1,131 @@
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
+import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
+
+type Props = {
+  isTopOfPage: boolean;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+export const Navbar = ({
+  isTopOfPage,
+  selectedPage,
+  setSelectedPage,
+}: Props) => {
+  const flexBetween = "flex items-center justify-between";
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const navbarBG = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
+
+  return (
+    <nav>
+      <div
+        className={`${navbarBG} ${flexBetween} fixed top-0 z-[100] w-full py-6`}
+      >
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* left side */}
+            <img src={Logo} alt="logo" />
+
+            {/* right side */}
+            {isAboveMediumScreens ? (
+              <div className={`${flexBetween} w-full`}>
+                <ul className={`${flexBetween} gap-8 text-sm capitalize`}>
+                  <li>
+                    <Link
+                      page="home"
+                      selectedPage={selectedPage}
+                      setSelectedPage={setSelectedPage}
+                    />
+                  </li>
+                  <li>
+                    <Link
+                      page="benefits"
+                      selectedPage={selectedPage}
+                      setSelectedPage={setSelectedPage}
+                    />
+                  </li>
+                  <li>
+                    <Link
+                      page="our classes"
+                      selectedPage={selectedPage}
+                      setSelectedPage={setSelectedPage}
+                    />
+                  </li>
+                  <li>
+                    <Link
+                      page="contact us"
+                      selectedPage={selectedPage}
+                      setSelectedPage={setSelectedPage}
+                    />
+                  </li>
+                </ul>
+                <div className={`${flexBetween} gap-8`}>
+                  <p className="capitalize">sign in</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
+                </div>
+              </div>
+            ) : (
+              <button
+                className=" rounded-full bg-secondary-500 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="h-6 w-6 text-white" />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      {/* mobile menu modal */}
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className=" fixed right-0 bottom-0 z-[110] h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          {/* close icon */}
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400 " />
+            </button>
+          </div>
+
+          {/* menu items */}
+          <ul className="ml-[33%] flex flex-col gap-10 text-2xl capitalize">
+            <li>
+              <Link
+                page="home"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </li>
+            <li>
+              <Link
+                page="benefits"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </li>
+            <li>
+              <Link
+                page="our classes"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </li>
+            <li>
+              <Link
+                page="contact us"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </li>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+};
